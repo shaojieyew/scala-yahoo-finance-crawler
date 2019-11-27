ee2951835ff31b28907fc9bfc281603ba613ce0f
Shao Jie <shaojieyew@gmail.com>
2019-11-28 00:53:52 +0800

    updadted script

--- YahooFinance/target/run.sh
+++ YahooFinance/target/run.sh
@@ -1,1 +1,3 @@
-java -cp  YahooFinance-1.0-SNAPSHOT-jar-with-dependencies.jar finance.App localhost:5432 postgres admin Data/Listing/SGX.csv print
+java -cp  YahooFinance-1.0-SNAPSHOT-jar-with-dependencies.jar finance.App localhost:5432 postgres admin Data/Listing print
+#java -cp  YahooFinance-1.0-SNAPSHOT-jar-with-dependencies.jar finance.App localhost:5432 postgres admin Data/Listing/US_LARGE_CAP.csv print seed
+#java -cp  YahooFinance-1.0-SNAPSHOT-jar-with-dependencies.jar finance.App localhost:5432 postgres admin Data/Listing/SGX.csv print
